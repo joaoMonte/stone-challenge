59adc2055c190a49fb632022761aea6e8ccca5f5
joao.monte <joao.monte@gprt.ufpe.br>
2019-11-24 22:12:18 -0300

    Created the default project files

---
+++ lib/stone_challenge.ex
@@ -0,0 +1,18 @@
+defmodule StoneChallenge do
+  @moduledoc """
+  Documentation for StoneChallenge.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StoneChallenge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
