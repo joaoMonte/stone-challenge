a78ceefad57ddfa59c1babdb2cb897a187ff3ee4
joao.monte <joao.monte@gprt.ufpe.br>
2019-11-30 20:42:34 -0300

    Implemented the method to transfer money between 2 accounts

--- lib/bank.ex
+++ lib/bank.ex
@@ -6,8 +6,30 @@ defmodule Bank do
   @doc """
   The two forms cover the transference to one or more accounts
   """
-  def transfer_money(sender, receiver, value) do
-    IO.puts "Transfering money from " <> receiver <> " to " <> sender
+  def debit_account(account, integer_value, fractionary_value) do
+    IO.puts "Account debited"
+  end
+
+  def credit_account(account, integer_value, fractionary_value) do
+    IO.puts "Account credited"
+  end
+
+  def transfer_money(sender, receiver, integer_value, fractionary_value) do
+    cond do
+      sender[:integer_balance] == integer_value ->
+        cond do
+          sender[:fractionary_balance] < fractionary_value ->
+            IO.puts "You don't have enough money to make this transfer! Aborting Operation ..."
+          true ->
+            debit_account(sender, integer_value, fractionary_value)
+            credit_account(receiver, integer_value, fractionary_value)
+        end
+      sender[:integer_balance] < integer_value ->
+        IO.puts "You don't have enough money to make this transfer! Aborting Operation ..."
+      true ->
+        debit_account(sender, integer_value, fractionary_value)
+        credit_account(receiver, integer_value, fractionary_value)
+    end
   end
 
   def transfer_money(sender, [first_receiver | other_receivers], value) do
