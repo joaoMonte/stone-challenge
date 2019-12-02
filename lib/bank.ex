6b76331a10aaacbd31aa8629bb984cf37d8c89a0
joao.monte <joao.monte@gprt.ufpe.br>
2019-12-02 10:43:27 -0300

    Updated the credit and debit methods to comply with the functional elixir paradigm

--- lib/bank.ex
+++ lib/bank.ex
@@ -7,24 +7,26 @@ defmodule Bank do
   Debit  and credit functions, which are called during the transfer function
   """
   def debit_account(account, integer_value, fractionary_value) do
-    account = %{account | integer_balance: account.integer_balance - integer_value}
-
     if account.fractionary_balance < fractionary_value do
-      account = %{account | integer_balance: account.integer_balance - 1}
-      account = %{account | fractionary_balance: account.fractionary_balance - fractionary_value + 100}
+      account = %{account | integer_balance: account.integer_balance - integer_value - 1}
+      %{account | fractionary_balance: account.fractionary_balance - fractionary_value + 100}
 
     else
-      account = %{account | fractionary_balance: account.fractionary_balance - fractionary_value}
+      account = %{account | integer_balance: account.integer_balance - integer_value}
+      %{account | fractionary_balance: account.fractionary_balance - fractionary_value}
     end
   end
 
   def credit_account(account, integer_value, fractionary_value) do
-    account = %{account | integer_balance: account.integer_balance + integer_value}
-    account = %{account | fractionary_balance: account.fractionary_balance + fractionary_value}
 
-    if account.fractionary_balance > 100 do
+    if account.fractionary_balance + fractionary_value > 100 do
+      account = %{account | fractionary_balance: account.fractionary_balance + fractionary_value}
       account = %{account | integer_balance: account.integer_balance + div(account.fractionary_balance, 100)}
-      account = %{account | fractionary_balance: rem(account.fractionary_balance, 100}
+      account = %{account | fractionary_balance: rem(account.fractionary_balance, 100)}
+      %{account | integer_balance: account.integer_balance + integer_value}
+    else
+      account = %{account | fractionary_balance: account.fractionary_balance + fractionary_value}
+      %{account | integer_balance: account.integer_balance + integer_value}
     end
   end
 
