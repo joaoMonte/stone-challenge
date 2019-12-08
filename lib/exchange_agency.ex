cc8eebdcad5c2519889d4488f745e3119862ec5a
joao.monte <joao.monte@gprt.ufpe.br>
2019-12-08 15:11:22 -0300

    Created the file which contains the module that deal exchange operations

---
+++ lib/exchange_agency.ex
@@ -0,0 +1,25 @@
+defmodule ExchangeAgency do
+  @moduledoc """
+  Module which encapsulate the currency exchange operation.
+  It deals with bank accounts
+  """
+
+  @jpy_to_cny [1,2]
+  @jpy_to_brl 1
+  @jpy_to_jod 3
+  @brl_to_jpy 4
+  @brl_to_cny 5
+  @brl_to_jod 6
+  @cny_to_brl 7
+  @cny_to_jpy 8
+  @cny_to_jod 9
+  @jod_to_brl 10
+  @jod_to_cny 11
+  @jod_to_jpy 12
+
+
+  def buy_yens(account, int_value, fract_value) do
+
+  end
+
+end
