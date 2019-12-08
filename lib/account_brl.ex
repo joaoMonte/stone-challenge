51ab49a316bcb69383205d18f9a0364ea434e8f8
joao.monte <joao.monte@gprt.ufpe.br>
2019-12-07 21:11:50 -0300

    Renamed account to account_brl, aiming to implement the support for different currencies. Updated the module documentation too, complying the new fields of the struct.

---
+++ lib/account_brl.ex
@@ -0,0 +1,14 @@
+defmodule AccountBRL do
+  @moduledoc """
+  Struct which abstracts an account information.
+  Each account has:
+  - An owner, who can made transactions with it
+  - The currency of the account balance
+  - The balance, which is divided into integer and fractional parts
+  - The number of decimals after comma
+  - The structure for all account types are equal
+  """
+
+  defstruct [:owner, decimals: 2, integer_balance: 0, fractionary_balance: 0, currency: "BRL"]
+
+end
