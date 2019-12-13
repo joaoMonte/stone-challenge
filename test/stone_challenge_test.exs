fed7f794d2d7865a7a89e4d4cedc5f39a59f0bee
joao.monte <joao.monte@gprt.ufpe.br>
2019-12-12 21:53:35 -0300

    Added a describe to encapsulate the accounts creation tests

--- test/stone_challenge_test.exs
+++ test/stone_challenge_test.exs
@@ -2,20 +2,22 @@ defmodule StoneChallengeTest do
   use ExUnit.Case
   doctest StoneChallenge
 
-  test "Creation of a BRL account" do
-    assert AccountFactory.factory("BRL", "Joao") == %AccountBRL{owner: "Joao", decimals: 2, integer_balance: 0, fractionary_balance: 0, currency: "BRL"}
-  end
+  describe "Creation of a " do
+    test "BRL account" do
+      assert AccountFactory.factory("BRL", "Joao") == %AccountBRL{owner: "Joao", decimals: 2, integer_balance: 0, fractionary_balance: 0, currency: "BRL"}
+    end
 
-  test "Creation of a CNY account" do
-    assert AccountFactory.factory("CNY", "Xing") == %AccountCNY{owner: "Xing", decimals: 1, integer_balance: 0, fractionary_balance: 0, currency: "CNY"}
-  end
+    test "CNY account" do
+      assert AccountFactory.factory("CNY", "Xing") == %AccountCNY{owner: "Xing", decimals: 1, integer_balance: 0, fractionary_balance: 0, currency: "CNY"}
+    end
 
-  test "Creation of a JPY account" do
-    assert AccountFactory.factory("JPY", "Uzumaki") == %AccountJPY{owner: "Uzumaki", decimals: 0, integer_balance: 0, fractionary_balance: 0, currency: "JPY"}
-  end
+    test "JPY account" do
+      assert AccountFactory.factory("JPY", "Uzumaki") == %AccountJPY{owner: "Uzumaki", decimals: 0, integer_balance: 0, fractionary_balance: 0, currency: "JPY"}
+    end
 
-  test "Creation of a JOD account" do
-    assert AccountFactory.factory("JOD", "Omar") == %AccountJOD{owner: "Omar", decimals: 3, integer_balance: 0, fractionary_balance: 0, currency: "JOD"}
+    test "JOD account" do
+      assert AccountFactory.factory("JOD", "Omar") == %AccountJOD{owner: "Omar", decimals: 3, integer_balance: 0, fractionary_balance: 0, currency: "JOD"}
+    end
   end
 
   test "Make a credit on a BRL account" do
