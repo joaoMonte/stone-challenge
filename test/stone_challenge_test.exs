52669bc27c8aa331eac7ec1a1fcd8a3ba5d0f417
joao.monte <joao.monte@gprt.ufpe.br>
2019-12-12 21:06:57 -0300

    Implemented tests to cover account creation

--- test/stone_challenge_test.exs
+++ test/stone_challenge_test.exs
@@ -2,7 +2,20 @@ defmodule StoneChallengeTest do
   use ExUnit.Case
   doctest StoneChallenge
 
-  test "greets the world" do
-    assert StoneChallenge.hello() == :world
+  test "Creation of a BRL account" do
+    assert AccountFactory.factory("BRL", "Joao") == %AccountBRL{owner: "Joao", decimals: 2, integer_balance: 0, fractionary_balance: 0, currency: "BRL"}
   end
+
+  test "Creation of a CNY account" do
+    assert AccountFactory.factory("CNY", "Xing") == %AccountCNY{owner: "Xing", decimals: 1, integer_balance: 0, fractionary_balance: 0, currency: "CNY"}
+  end
+
+  test "Creation of a JPY account" do
+    assert AccountFactory.factory("JPY", "Uzumaki") == %AccountJPY{owner: "Uzumaki", decimals: 0, integer_balance: 0, fractionary_balance: 0, currency: "JPY"}
+  end
+
+  test "Creation of a JOD account" do
+    assert AccountFactory.factory("JOD", "Omar") == %AccountJOD{owner: "Omar", decimals: 3, integer_balance: 0, fractionary_balance: 0, currency: "JOD"}
+  end
+
 end
