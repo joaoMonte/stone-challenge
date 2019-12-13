243bb5c57e9cdea72520ae8425a8fb27099275ed
joao.monte <joao.monte@gprt.ufpe.br>
2019-12-12 21:32:54 -0300

    Fixed english error on tests description

--- test/stone_challenge_test.exs
+++ test/stone_challenge_test.exs
@@ -18,48 +18,71 @@ defmodule StoneChallengeTest do
     assert AccountFactory.factory("JOD", "Omar") == %AccountJOD{owner: "Omar", decimals: 3, integer_balance: 0, fractionary_balance: 0, currency: "JOD"}
   end
 
-  test "Make an credit on a BRL account" do
+  test "Make a credit on a BRL account" do
     joao = AccountFactory.factory("BRL", "Joao")
     joao = Bank.credit_account(joao, 30, 20)
     assert joao == %AccountBRL{owner: "Joao", decimals: 2, integer_balance: 30, fractionary_balance: 20, currency: "BRL"}
   end
 
-  test "Make an credit on a CNY account" do
+  test "Make a credit on a CNY account" do
     xing = AccountFactory.factory("CNY", "Xing")
     xing = Bank.credit_account(xing, 30, 2)
     assert xing == %AccountCNY{owner: "Xing", decimals: 1, integer_balance: 30, fractionary_balance: 2, currency: "CNY"}
   end
 
-  test "Make an credit on a JPY account" do
+  test "Make a credit on a JPY account" do
     uzumaki = AccountFactory.factory("JPY", "Uzumaki")
     uzumaki = Bank.credit_account(uzumaki, 30, 0)
     assert uzumaki == %AccountJPY{owner: "Uzumaki", decimals: 0, integer_balance: 30, fractionary_balance: 0, currency: "JPY"}
   end
 
-  test "Make an credit on a JOD account" do
+  test "Make a credit on a JOD account" do
     omar = AccountFactory.factory("JOD", "Omar")
     omar = Bank.credit_account(omar, 30, 300)
     assert omar == %AccountJOD{owner: "Omar", decimals: 3, integer_balance: 30, fractionary_balance: 300, currency: "JOD"}
   end
 
-  test "Make an credit with carry on a BRL account" do
+  test "Make a credit with carry on a BRL account" do
     joao = %AccountBRL{owner: "Joao", decimals: 2, integer_balance: 30, fractionary_balance: 20, currency: "BRL"}
     joao = Bank.credit_account(joao, 10, 90)
     assert joao == %AccountBRL{owner: "Joao", decimals: 2, integer_balance: 41, fractionary_balance: 10, currency: "BRL"}
   end
 
-  test "Make an credit with carry on a CNY account" do
+  test "Make a credit with carry on a CNY account" do
     xing = %AccountCNY{owner: "Xing", decimals: 1, integer_balance: 30, fractionary_balance: 2, currency: "CNY"}
     xing = Bank.credit_account(xing, 20, 9)
     assert xing == %AccountCNY{owner: "Xing", decimals: 1, integer_balance: 51, fractionary_balance: 1, currency: "CNY"}
   end
 
-  test "Make an credit with carry on a JOD account" do
+  test "Make a credit with carry on a JOD account" do
     omar = %AccountJOD{owner: "Omar", decimals: 3, integer_balance: 30, fractionary_balance: 300, currency: "JOD"}
     omar = Bank.credit_account(omar, 30, 900)
     assert omar == %AccountJOD{owner: "Omar", decimals: 3, integer_balance: 61, fractionary_balance: 200, currency: "JOD"}
   end
 
+  test "Make a credit on a BRL account" do
+    joao = AccountFactory.factory("BRL", "Joao")
+    joao = Bank.credit_account(joao, 30, 20)
+    assert joao == %AccountBRL{owner: "Joao", decimals: 2, integer_balance: 30, fractionary_balance: 20, currency: "BRL"}
+  end
+
+  test "Make a credit on a CNY account" do
+    xing = AccountFactory.factory("CNY", "Xing")
+    xing = Bank.credit_account(xing, 30, 2)
+    assert xing == %AccountCNY{owner: "Xing", decimals: 1, integer_balance: 30, fractionary_balance: 2, currency: "CNY"}
+  end
+
+  test "Make a credit on a JPY account" do
+    uzumaki = AccountFactory.factory("JPY", "Uzumaki")
+    uzumaki = Bank.credit_account(uzumaki, 30, 0)
+    assert uzumaki == %AccountJPY{owner: "Uzumaki", decimals: 0, integer_balance: 30, fractionary_balance: 0, currency: "JPY"}
+  end
+
+  test "Make a credit on a JOD account" do
+    omar = AccountFactory.factory("JOD", "Omar")
+    omar = Bank.credit_account(omar, 30, 300)
+    assert omar == %AccountJOD{owner: "Omar", decimals: 3, integer_balance: 30, fractionary_balance: 300, currency: "JOD"}
+  end
 
 
 end
